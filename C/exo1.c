354aebe7e96fff16eafc2ae8e0120a3f3a0ccb7f
whostmdz <141586328+whostmdz@users.noreply.github.com>
2026-02-12 19:12:36 +0100

    Change loop initialization to start from 0

--- C/exo1.c
+++ C/exo1.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
   int i;
-  for(i=1; i < argc; i++)
+  for(i=0; i < argc; i++)  // i is initialised to 0 to get the first
   {
     printf("argv[%d] = %s\n",i,argv[i]);
   }
