2fada4d39291f8d362928d8d971644f3165c7e4e
whostmdz <thomasdiemoz@gmail.com>
2026-02-13 13:58:14 +0100

    count_char - Week 1

---
+++ C/count_char.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int count_char(const char *s, char c){
+  int i;
+  int cpt=0;
+  for(i=0; s[i]!='\0'; i++){
+    if(c == s[i]){
+      cpt++;
+    }
+  }
+  return cpt;
+}
+
+int main(){
+  printf("%d\n", count_char("banana", 'a'));
+  return 0;
+}
